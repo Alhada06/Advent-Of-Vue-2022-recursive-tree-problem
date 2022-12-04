cb791247a7f4b3acf4013c5f3c328cb4a8a16372
Diogo Alhada <93165102+Alhada06@users.noreply.github.com>
2022-12-04 16:33:25 +0000

    solutions, so simple but a head scratcher

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
+
+<script setup>
+import ChristmasTree from './ChristmasTree.vue'
+</script>
+
+
+
 <template>
   <div class="w-full h-full flex justify-center items-center">
     <ChristmasTree :size="7" />
   </div>
 </template>
-
-<script setup>
-import ChristmasTree from './ChristmasTree.vue'
-</script>
